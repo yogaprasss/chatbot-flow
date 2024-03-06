0c258c4c50b912f45847b61357e3c82e90298756
yoga <prasetyo.yoga1109@gmail.com>
2024-03-06 09:47:56 +0700

    initial layout

---
+++ src/views/LeftSideBar.tsx
@@ -0,0 +1,9 @@
+const LeftSideBar = () => {
+  return (
+    <div className='w-72 h-screen shadow-md bg-white'>
+      Left Side Bar
+    </div>
+  );
+};
+
+export default LeftSideBar;
---
+++ src/views/RightSideBar.tsx
@@ -0,0 +1,9 @@
+const RightSideBar = () => {
+  return (
+    <div className='w-72 h-screen shadow-md bg-white'>
+      Right Side Bar
+    </div>
+  );
+};
+
+export default RightSideBar;
